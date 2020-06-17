a91997c4a99d5f3619457c221fb9da8ba816097a
Alvarao <alvgaona@gmail.com>
2020-06-17 03:31:54 +0000

    fix: Update scripts

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "roslaunch my_robot turtlebot_timsWorld.launch" &
+xterm -e "roslaunch my_robot world.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
